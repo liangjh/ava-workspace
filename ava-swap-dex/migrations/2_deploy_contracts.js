b7102321f93a8f757916101f9f9ba36a25135538
Jonathan Liang <jonathanl@squareup.com>
2022-10-22 15:28:47 -0500

    chained contract deploy

--- ava-swap-dex/migrations/2_deploy_contracts.js
+++ ava-swap-dex/migrations/2_deploy_contracts.js
@@ -3,12 +3,12 @@ const AvaSwap = artifacts.require("AvaSwap");
 
 module.exports = async function(deployer) {
 
-    await deployer.deploy(DevToken);
-    const devToken = await DevToken.deployed();
+    deployer.deploy(DevToken).then(function() {
+        return deployer.deploy(AvaSwap, DevToken.address)
+    })
 
-    await deployer.deploy(AvaSwap, devToken.address);
-    const avaSwap = await AvaSwap.deployed()
+    // const devToken = await deployer.deploy(DevToken).deployed();
+    // const avaSwap = await deployer.deploy(AvaSwap, devToken.address).deployed();
+    // await devToken.transfer(avaSwap.address, '1000000000000000000000000');
 
-    await devToken.transfer(avaSwap.address, '1000000000000000000000000');
 }
-
